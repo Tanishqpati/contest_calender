dd271fce63d1b7831f05cb7b30ecfddf7c945b05
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-05-17 08:31:15 +0530

    feat: Add route to add contests to Google Calendar

--- contest-calender-backend/routes/contests.js
+++ contest-calender-backend/routes/contests.js
@@ -23,4 +23,47 @@ const fetchCodeChefContests = async () => {
     }
 }
 
+router.get('/add', async (req, res) => {
+    const oAuth2Client = new google.auth.OAuth2();
+    oAuth2Client.setCredentials(req.session.tokens);
+
+    const calender = google.calendar({ version: 'v3', auth: oAuth2Client});
+
+    const leetCodeContests = await fetchLeetcodeContests();
+    const codeChefContests = await fetchCodeChefContests();
+
+    const addEvent = async (contest) => {
+        try {
+            await calender.events.insert({
+                calendarId: 'primary',
+                resource: {
+                    summary: contest.title || contest.name,
+                    start: {
+                        dateTime: contest.start_time || contest.start_date,
+                        timeZone: 'UTC',
+                    },
+                    end: {
+                        dateTime: contest.end_time || contest.end_date,
+                        timeZone: 'UTC',
+                    },
+                    reminders: {
+                        useDefault: false,
+                        overrides: [
+                            { method: 'email', minutes: 24 * 60 },
+                            { method: 'popup', minutes: 10 },
+                        ],
+                    },
+                },
+            })
+        } catch (error) {
+            console.error('Error adding event to calendar:', error);
+        }
+    };
+
+    leetCodeContests.forEach(addEvent);
+    codeChefContests.forEach(addEvent);
+
+    res.json({ status: 'success' });
+});
+
 module.exports = router;
